618f9ff208bc67325507f7e8bd3fd7b9fb697eb1
MrJanHorak <janny_b_goode@hotmail.com>
2024-09-05 21:47:31 -0400

    remove no longer needed libraries

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-// const puppeteer = require('puppeteer');
-// const NodeCache = require('node-cache');
 const express = require('express');
 const cors = require('cors');
 const app = express();
 
 app.use(cors({ origin: '*' }));
-// Create a new cache instance
-// const myCache = new NodeCache({ stdTTL: 24 * 60 * 60, checkperiod: 120 });
 
 let url = 'https://www.nytimes.com/games/strands';
 let url2 = 'https://www.nytimes.com/svc/strands/v2/';
@@ -31,40 +27,12 @@ app.get('/data', async (req, res) => {
   try {
     const response = await fetch(dynamicUrl);
     jsonResponse = await response.json();
-
   } catch (error) {
     console.error('Error fetching JSON data:', error);
     return res.status(500).json({ error: 'Failed to fetch JSON data' });
   }
 
   const { themeWords, spangram, startingBoard, clue } = jsonResponse;
-
-  // const browser = await puppeteer.launch({
-  //   args: ['--no-sandbox'],
-  // });
-
-  // const page = await browser.newPage();
-  // await page.emulateTimezone(clientTimezone);
-  // await page.goto(url);
-
-  // // Click the play button
-  // await page.click('button.Feo8La_playButton');
-
-  // // Wait for the necessary element to be loaded
-  // await page.waitForSelector('button.pRjvKq_item');
-
-  // // Scrape the data
-  // data = await page.evaluate(() => {
-  //   const buttons = Array.from(document.querySelectorAll('button.pRjvKq_item'));
-  //   const buttonValues = buttons.map((button) => button.innerText);
-  //   const clue = document.querySelector('h1.umfyna_clue').innerText;
-  //   return { buttonValues, clue };
-  // });
-
-  // // myCache.set('data', data);
-
-  // await browser.close();
-  // // }
 
   // Format the data into an array of arrays with 8 arrays of 6 numbers
   let formattedButtonValues = [];
